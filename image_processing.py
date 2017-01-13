f87049f18856ef22bf6b025b4ae22b2074d224cc
Giang Bui <giangbui@users.noreply.github.com>
2017-01-13 17:17:26 -0600

    Create image_processing.py

---
+++ image_processing.py
@@ -0,0 +1,5 @@
+img = Image.open(join(mypath, f)).convert('RGB')
+ycbcr = img.convert('YCbCr')
+W,H = ycbcr.size
+highRes = np.array(ycbcr)*1.0/255
+ycbcr = ycbcr.resize((int(W*1.0/3),int(H*1.0/3)),resample=PIL.Image.LANCZOS)
