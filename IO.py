e5952956e0ed3e506aa9ddfba66b18d1528f9ebf
Giang Bui <giangbui@users.noreply.github.com>
2017-01-13 17:24:42 -0600

    Update IO.py

--- IO.py
+++ IO.py
@@ -1,3 +1,15 @@
 from os import listdir
 from os.path import isfile, join
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+###########################################################################
+
+def load_hdf5_data():    
+    with h5py.File('./preprocessing_code/mul_image_train.h5','r') as hf:
+        print('List of arrays in this file: \n', hf.keys())
+        data = np.array(hf.get('data'))
+        label = np.array(hf.get('label'))
+        # read a half only
+        data = data[::2,:,:,:]
+        label = label[::2,:,:,:]        
+    return data,label
