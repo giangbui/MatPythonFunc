aa5d1674f98ad612f41d5635265ac100cb03d580
Giang Bui <giangbui@users.noreply.github.com>
2017-01-13 17:38:50 -0600

    Update IO.py

--- IO.py
+++ IO.py
@@ -1,9 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
 ###########################################################################
-
 def load_hdf5_data():    
     with h5py.File('./preprocessing_code/mul_image_train.h5','r') as hf:
         print('List of arrays in this file: \n', hf.keys())
@@ -13,3 +11,4 @@ def load_hdf5_data():
         data = data[::2,:,:,:]
         label = label[::2,:,:,:]        
     return data,label
+###########################################################################
